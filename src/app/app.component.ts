2429968d608dba7fd0b97b507b1c4965bab44424
anaysoul <soulyana_lakew@outlook.com>
2022-03-26 22:45:24 -0400

    memory leaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ModalService } from './services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'clips';
+  showModal = true;
+
+  constructor(public modal: ModalService) {}
+
+  ngOnInit() {
+    setInterval(() => {
+      (this.showModal = !this.showModal), 1000;
+    });
+  }
 }
