641135df725cab0fb2bf458defdbfc22e19fcd82
anaysoul <soulyana_lakew@outlook.com>
2022-03-27 18:21:47 -0400

    designing an input component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,22 @@ import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../services/modal.service';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
   imports: [CommonModule],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
   providers: [ModalService],
 })
 export class SharedModule {}
