16b254e44e641256a9db8cd188a12c9702c388d9
anaysoul <soulyana_lakew@outlook.com>
2022-03-29 20:14:04 -0400

    importing AngularFire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA-20TepgQ6cUS9SSQueTyNxCt4WwQy2g4',
+    authDomain: 'clips-cec97.firebaseapp.com',
+    projectId: 'clips-cec97',
+    storageBucket: 'clips-cec97.appspot.com',
+    appId: '1:897177906315:web:1312e8d4f8233b9219788e',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA-20TepgQ6cUS9SSQueTyNxCt4WwQy2g4',
+    authDomain: 'clips-cec97.firebaseapp.com',
+    projectId: 'clips-cec97',
+    storageBucket: 'clips-cec97.appspot.com',
+    appId: '1:897177906315:web:1312e8d4f8233b9219788e',
+  },
 };
 
 /*
