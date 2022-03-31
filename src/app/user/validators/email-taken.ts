bddddf971869a5447190a5f3fb524a548d13cff6
anaysoul <soulyana_lakew@outlook.com>
2022-03-30 22:19:59 -0400

    validating unique emails

--- src/app/user/validators/email-taken.ts
+++ src/app/user/validators/email-taken.ts
@@ -7,11 +7,15 @@ import {
 } from '@angular/forms';
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class EmailTaken implements AsyncValidator {
   constructor(private auth: AngularFireAuth) {}
 
-  validate(
-    control: AbstractControl
-  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {}
+  validate = (control: AbstractControl): Promise<ValidationErrors | null> => {
+    return this.auth
+      .fetchSignInMethodsForEmail(control.value)
+      .then((response) => (response.length ? { emailTaken: true } : null));
+  };
 }
