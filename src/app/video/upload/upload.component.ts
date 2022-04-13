4a89e33b19cf64b1180d552185dd2d73d2d892c1
anaysoul <soulyana_lakew@outlook.com>
2022-04-12 21:45:03 -0400

    uploading files with firebase

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-upload',
@@ -16,7 +18,7 @@ export class UploadComponent implements OnInit {
     title: this.title,
   });
 
-  constructor() {}
+  constructor(private storage: AngularFireStorage) {}
 
   ngOnInit(): void {}
 
@@ -35,6 +37,9 @@ export class UploadComponent implements OnInit {
   }
 
   uploadFile() {
-    console.log('File uploaded');
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+
+    this.storage.upload(clipPath, this.file);
   }
 }
